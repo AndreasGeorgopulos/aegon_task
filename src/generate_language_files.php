594ef765b193a4d2272c38e47be4bcf755f14ed7
Georgopulos Andreas <andreas.georgopulos@kryonet.hu>
2023-04-02 16:56:57 +0200

    Catch exception.

--- src/generate_language_files.php
+++ src/generate_language_files.php
@@ -1,9 +1,19 @@
 <?php
 
+use Language\LanguageBatchBo;
+
 chdir(__DIR__);
 
 include('../vendor/autoload.php');
 
-$languageBatchBo = \Language\LanguageBatchBo::getInstance();
-$languageBatchBo->generateLanguageFiles();
-$languageBatchBo->generateAppletLanguageXmlFiles();
+try {
+	$languageBatchBo = LanguageBatchBo::getInstance();
+	$languageBatchBo->generateLanguageFiles();
+	$languageBatchBo->generateAppletLanguageXmlFiles();
+
+} catch (Exception $exception) {
+	printf('Error: %s%s', $exception->getMessage(), PHP_EOL);
+	printf('File: %s%s', $exception->getFile(), PHP_EOL);
+	printf('Line: %s%s', $exception->getLine(), PHP_EOL);
+
+}
